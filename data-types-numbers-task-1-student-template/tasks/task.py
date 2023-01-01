cd2a8c3a76a07d123dcfb272929d8d918fec292f
cara <120713656+quareeem@users.noreply.github.com>
2023-01-01 17:43:29 +0600

    Add files via upload

---
+++ data-types-numbers-task-1-student-template/tasks/task.py
@@ -0,0 +1,8 @@
+from typing import Union
+
+NumType = Union[int, float]
+
+
+def some_expression_with_rounding(a: NumType, b: NumType) -> NumType:
+    result = round((12 * a + 25 * b) / (1 + a ** (2 ** b)), 2)
+    return result
